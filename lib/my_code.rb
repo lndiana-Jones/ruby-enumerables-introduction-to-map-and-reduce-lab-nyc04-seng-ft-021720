6ac626e774304bbf055c0957e6446ace3a85f9e9
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-07 04:57:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # My Code here....
+def map_tonegativize(source_array)
+  i = 0
+  neg_arr = []
+  while i < source_array.length
+    neg_arr << source_array[i] * -1
+    i += 1 
+  end
+  neg_arr
+end
