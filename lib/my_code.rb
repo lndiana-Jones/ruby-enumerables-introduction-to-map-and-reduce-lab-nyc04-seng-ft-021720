ca1a771b8e9869fad882310223823ced8385ea8b
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-07 05:00:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,12 @@ def map_to_negativize(source_array)
   end
   neg_arr
 end
+
+def map_to_no_change(source_array)
+  i = 0 
+  while i < source_array.length 
+    source_array
+    i += 1 
+  end
+  source_array
+end 
